4e9b4615c6036db688a047c66cacca8e09193611
miraDask <mira.daskalov@gmail.com>
2019-06-13 23:09:36 +0300

    Initial commit

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -2,13 +2,8 @@ const secondsHand = document.querySelector('.second-hand');
 const minHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
-function calcDegrees(timeUnit) {
-    let totalUnits = 60;
-    if (timeUnit === 'hours') {
-        totalUnits = 12;
-    }
-
-    return ((timeUnit / totalUnits) * 360) + 90;
+function calcDegrees(timeUnit, divisor) {
+    return ((timeUnit / divisor) * 360) + 90;
 }
 
 function setDate() {
@@ -17,14 +12,16 @@ function setDate() {
     const seconds = now.getSeconds();
     const minutes = now.getMinutes();
     const hours = now.getHours();
-    const secondsDegrees = calcDegrees(seconds);
-    const minutesDegrees = calcDegrees(minutes);
-    const hoursDegrees = calcDegrees(hours);
+    const secondsDegrees = calcDegrees(seconds, 60);
+    const minutesDegrees = calcDegrees(minutes, 60);
+    const hoursDegrees = calcDegrees(hours, 12);
 
     secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
     minHand.style.transform = `rotate(${minutesDegrees}deg)`;
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+    secondsHand.style.transition = secondsDegrees === 90 ? 'all 0s' : 'all 0.05s';
+    minHand.style.transition = minutesDegrees === 90 ? 'all 0s' : 'all 0.05s';
 }
 
 setInterval(setDate, 1000);
-setDate();
